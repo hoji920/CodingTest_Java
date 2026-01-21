a47b995b228b9b9be6bd7c99b0cd97d63f95de17
hoji920 <50348064+hoji920@users.noreply.github.com>
2026-01-21 15:30:40 +0900

    [level 0] Title: 수 조작하기 1, Time: 74.64 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        
+        String[] arr = control.split("");
+        char[] charArr = control.toCharArray();
+        for(int i=0; i < charArr.length; i++){
+            if(charArr[i] == 'w') 
+                n += 1;
+            else if(charArr[i] == 's') 
+                n -= 1;
+            else if(charArr[i] =='d') 
+                n += 10;
+            else  
+                n -= 10;
+        }
+        answer = n;
+        return answer;
+    }
+}
