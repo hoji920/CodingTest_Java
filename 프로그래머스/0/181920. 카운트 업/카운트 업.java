f7e526c8dc148962351aef1950eefdcf6e18c35a
hoji920 <50348064+hoji920@users.noreply.github.com>
2026-01-21 15:57:20 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,11 +2,10 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         
         int[] answer = new int[end_num-start_num+1];
-        answer[0]=start_num;
-        
-        for(int i=1; i<answer.length; i++){
-            start_num+=1;
+       
+        for(int i=0; i<answer.length; i++){
             answer[i]= start_num;
+            start_num++;
         }
         return answer;
     }
